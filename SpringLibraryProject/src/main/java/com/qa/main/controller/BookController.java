7af9ccc0b09505959557e616db7bc7b1d19bdad4
Kashif <kashif-18@hotmail.com>
2021-10-04 10:46:49 +0100

    Controller class complete. Fixed tags for other classes

--- SpringLibraryProject/src/main/java/com/qa/main/controller/BookController.java
+++ SpringLibraryProject/src/main/java/com/qa/main/controller/BookController.java
@@ -1,5 +1,57 @@
 package com.qa.main.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.main.domain.Book;
+import com.qa.main.service.BookService;
+
+@RestController
+@RequestMapping("/library")
 public class BookController {
 
+	private BookService service;
+	
+	//http://localhost:9003/library/	
+	
+	//CREATE
+	@PostMapping("/createBook")
+	public Book createBook(@RequestBody Book book){
+		return this.service.createBook(book);
+	}
+	
+	
+	//READALL
+	@GetMapping("/getAllBooks")
+	public List<Book> getAllBooks(){
+		return this.service.getAllBooks();
+	}
+	
+	//READ
+	@GetMapping("/getBook/{id}")
+	public Book getBook(@PathVariable Long id) {
+		return this.service.getBook(id);
+	}
+	
+	//UPDATE
+	@PutMapping("/updateBook/{id}")
+	public Book updateBook(@PathVariable Long id,@RequestBody Book updatedBook) {
+		return this.service.updateBook(id, updatedBook);
+	}
+
+	//DELETE
+	@DeleteMapping("/deleteBook/{id}")
+	public boolean deleteBook(@PathVariable Long id) {
+		return this.service.deleteBook(id);
+	}
 }
--- SpringLibraryProject/src/main/java/com/qa/main/repo/BookRepo.java
+++ SpringLibraryProject/src/main/java/com/qa/main/repo/BookRepo.java
@@ -1,10 +1,11 @@
 package com.qa.main.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qa.main.domain.Book;
 
-
+@Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
 
 }
